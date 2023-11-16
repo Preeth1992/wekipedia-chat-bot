c8501e0375af2c2590974208fabf7071440acc7d
Preeth1992 <149002241+Preeth1992@users.noreply.github.com>
2023-11-16 18:11:58 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -12,10 +12,7 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 from dotenv import load_dotenv
 
 load_dotenv()
-# Get API key 
-OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
-# Set OpenAI API key
-openai.api_key = OPENAI_API_KEY
+
 
 with st.sidebar:
     st.title('PDF Chat App')
